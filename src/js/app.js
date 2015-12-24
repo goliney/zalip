7f9ba013b45457a99d0e128f191860f5b3e6373e
Sergey Goliney <sergey@goliney.com>
2015-12-24 19:47:53 +0200

    Matrix of cubes

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,61 @@
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-context.globalCompositeOperation = 'destination-atop';
+(function(){
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  context.globalCompositeOperation = 'destination-atop';
 
-var Point = Isomer.Point;
-var Path = Isomer.Path;
-var Shape = Isomer.Shape;
+  var iso;
+  var Point = Isomer.Point;
+  var Path = Isomer.Path;
+  var Shape = Isomer.Shape;
 
-var scale = 0.5;
+  var scale = 0.5;
+  var cubeWidth = 62;
+  var cubeHeight = 71;
+  var cubeEdge = 37;
+  var dimension;
 
-init();
-
-function init() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  var iso = new Isomer(canvas, {
-    originX: canvas.width / 2,
-    originY: canvas.height / 2
-  });
-
-  iso.add(cube(0, 0, 0));
-  iso.add(cube(0, 2, 0));
-  iso.add(cube(2, 0, 0));
-}
-
-function cube (x, y, z) {
-  return Shape.Prism(new Point(x, y, z)).scale(Point.ORIGIN, scale);
-}
-
-window.addEventListener('resize', function(event){
   init();
-});
+
+  function init() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    // make it square
+    var maxDimension = Math.max(canvas.width, canvas.height);
+
+    var diameter = Math.sqrt(Math.pow(maxDimension, 2) * 2);
+
+    // dimension of matrix
+    dimension = Math.ceil(diameter / (cubeEdge * 2)) + 5; // 5 - extra
+
+    // always even dimension
+    dimension = (dimension & 1) ? dimension : dimension + 1;
+
+    iso = new Isomer(canvas, {
+      originX: canvas.width / 2,
+      originY: (canvas.height / 2) + (cubeEdge)
+    });
+
+    drawField();
+  }
+
+  function cube (x, y, z) {
+    return Shape.Prism(new Point(x, y, z)).scale(Point.ORIGIN, scale);
+  }
+
+  function drawField() {
+    var center = Math.floor(dimension / 2);
+
+    for (var x = 0; x < dimension; x++) {
+      for (var y = 0; y < dimension; y++) {
+        iso.add(cube((x - center) * 2, (y - center) * 2, 0));
+      }
+    }
+
+    // iso.add(cube(0, 0, 0));
+  }
+
+  window.addEventListener('resize', function(event){
+    init();
+  });
+})();
