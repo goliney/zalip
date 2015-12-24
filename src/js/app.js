b4d14628ae7812ec59c3477427ffa4ca479cc025
Sergey Goliney <sergey@goliney.com>
2015-12-25 00:53:25 +0200

    Laggy rotations

--- src/js/app.js
+++ src/js/app.js
@@ -8,15 +8,21 @@
   var Path = Isomer.Path;
   var Shape = Isomer.Shape;
 
-  var scale = 0.5;
+  var scale = 0.75;
   var cubeWidth = 62;
   var cubeHeight = 71;
   var cubeEdge = 37;
   var dimension;
+  var center;
+
+  var animationTimer;
+  var angle;
 
   init();
 
   function init() {
+    clearTimeout(animationTimer);
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
@@ -31,29 +37,44 @@
     // always even dimension
     dimension = (dimension & 1) ? dimension : dimension + 1;
 
+    center = Math.floor(dimension / 2);
+
     iso = new Isomer(canvas, {
       originX: canvas.width / 2,
       originY: (canvas.height / 2) + (cubeEdge)
     });
 
-    drawField();
+    angle = 0;
+    animationTimer = setInterval(animate, 1000/30);
   }
 
   function cube (x, y, z) {
-    return Shape.Prism(new Point(x, y, z)).scale(Point.ORIGIN, scale);
+    return Shape.Prism(new Point(x, y, z));
   }
 
-  function drawField() {
-    var center = Math.floor(dimension / 2);
+  function animate() {
+      iso.canvas.clear();
 
-    for (var x = 0; x < dimension; x++) {
-      for (var y = 0; y < dimension; y++) {
-        iso.add(cube((x - center) * 2, (y - center) * 2, 0));
+      for (var x = 0; x < dimension; x++) {
+        for (var y = 0; y < dimension; y++) {
+          var dx = (x - center) * 2;
+          var dy = (y - center) * 2;
+          iso.add(
+            cube(dx, dy, 0)
+            .rotateZ(Point(dx + 0.5, dy + 0.5, 0.5), angle * Math.PI / 180)
+            .scale(Point.ORIGIN, scale)
+          );
+
+        }
       }
-    }
-
-    // iso.add(cube(0, 0, 0));
+      angle = angle === 180 ? 2 : angle + 2;
   }
+
+  // easeInCirc
+  // t - current time, b - start value, c - change in value, d - duration
+  function easing(t, b, c, d) {
+		return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
+	};
 
   window.addEventListener('resize', function(event){
     init();
